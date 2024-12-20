a5fca8bb9b10e28b9958f0a4ef5f1110e6a20c9c
Alejandro Monroy Muñoz <mon@MacBook-Pro-de-Alejandro.local>
2024-12-20 18:04:07 +0100

    Test notebook

--- docs/conf.py
+++ docs/conf.py
@@ -62,3 +62,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc={'figure.dpi': 96}",
+]
